5a4771210f0af2db3d4887c88de594c376accf6a
Nikunj <shahnikunj204@gmail.com>
2021-07-17 05:29:19 +0000

    LAst Commit

---
+++ src/test/StringFunctionsTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StringFunctionsTest {
+	
+	@Test
+	public void test1() {
+		
+		assertTrue(StringFunctions.isPalindrome("madam"));
+		
+	}
+
+}
